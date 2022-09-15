7edfaee7a4dac5b2f7e4b66014919ba30c488721
12179 <12179@DESKTOP-C4B4UTT.dhcp.wustl.edu>
2022-09-15 12:40:11 -0500

    Finished Part 1

--- src/studio2/Ruin.java
+++ src/studio2/Ruin.java
@@ -11,27 +11,53 @@ public class Ruin {
 		double startAmount = in.nextDouble();
 		double winChance = in.nextDouble();
 		double winLimit = in.nextDouble();
-		while (startAmount<winLimit && 0<startAmount)
+		double totalSimulations = in.nextDouble();
+		double Loss = in.nextDouble();
+		double Win = in.nextDouble();
+		
+		while (totalSimulations!=500)
+		{
+		double currentAmount = startAmount;
+		
+		while (currentAmount<winLimit && 0<currentAmount)
 		{
 		 double Hand = Math.random();
 		 boolean Lose = (Hand>winChance);
 			if (!Lose)
 			{	
-			startAmount++;
+			currentAmount++;
 			}
 			else 
 			{
-			startAmount--;
+			currentAmount--;
 			}}
-		if (startAmount>=winLimit)
+		if (currentAmount>=winLimit)
 		{
-			System.out.println("Today was Successful");
+			System.out.println("Today was Successful: Day "+totalSimulations);
+			Win++;
 		}
 		else 
 		{
-			System.out.println("Today was Ruinous");
+			System.out.println("Today was Ruinous: Day "+totalSimulations);
+			Loss++;
 		}
-		
+		totalSimulations++;
+		}
+		System.out.println("Number of Wins: " + Win);
+		System.out.println("Total Simulations" + totalSimulations);
+		double ruinRate = (Loss/(Loss+Win));
+		System.out.println("Ruin Rate from Simulation = "+ruinRate);
+		if (winChance==.5)
+		{
+			double expectedRuin = (1-(startAmount/winLimit));
+			System.out.println("Expected Ruin Rate: "+expectedRuin);
+		}
+		else 
+		{
+			double Alpha = ((1-winChance)/winChance);
+			double expectedRuin = ((Math.pow(Alpha,startAmount)-Math.pow(Alpha, winLimit))/(1-Math.pow(Alpha, winLimit)));
+			System.out.println("Expected Ruin Rate: "+expectedRuin);
+		}
 	}
 
 }
